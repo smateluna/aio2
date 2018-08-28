d3380b2927c465262b733580a1a5612fdd1d3dc5
evillar <evillar@conservador.cl>
2018-08-28 13:32:43 -0300

    se elimina sysout

--- src/cl/cbr/log/sentry/MyForwardedAddressResolver.java
+++ src/cl/cbr/log/sentry/MyForwardedAddressResolver.java
@@ -15,7 +15,6 @@ public class MyForwardedAddressResolver implements RemoteAddressResolver {
 	private BasicRemoteAddressResolver basicRemoteAddressResolver = new BasicRemoteAddressResolver();
 
 	private static String firstAddress(String csvAddrs) {
-		System.out.println("Resolviendo direccion");
 		List<String> ips = Arrays.asList(csvAddrs.split(","));
 		return ips.get(0).trim();
 	}
