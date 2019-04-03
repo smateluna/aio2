b19a9de85d8dbbfa5ffad16ed525e3e4bcfb1422
cbravo <cbravo@soporte03.cbrs.local>
2019-04-03 13:49:43 -0300

    fix plantillero

--- src/main/webapp/app/scripts/controllers/plantillero.js
+++ src/main/webapp/app/scripts/controllers/plantillero.js
@@ -40,19 +40,8 @@ app.controller('PlantilleroCtrl', function ($log, $rootScope, $scope, $modal, $t
 			title: null,
 			msg: null
 	};
-
-//	taOptions.toolbar = [
-//	                     //['h4', 'h5', 'h6', 'p',],
-//	                     //['italics'],
-//	                     //['justifyLeft', 'justifyCenter', 'justifyRight']
-//	                     //,['html']
-//	                     ];
 	
-	  taOptions.toolbar = [['html']];
-	
-	$scope.textAreaSetup = function($element){
-	  $element.attr('ui-codemirror', '');
-	};	
+	taOptions.toolbar = [['html']];
 
 	$scope.obtenerTiposCertificadosPorPerfil = function(){
 		$scope.openLoadingModal('Cargando Listado...', '');
